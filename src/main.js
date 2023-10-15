6f461b6e42022e5cd6f6679e7d48c07b73f68bfe
Jesus Mendoza <ccredulidad@gmail.com>
2023-10-14 22:39:37 -0500

    Add dark mode

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import './style.css'
 
+const dataset = document.documentElement.dataset;
+document.querySelector("#darkmode-selector").addEventListener("click", e => {
+    dataset.theme = (dataset.theme !== "dark") ? "dark" : "light";
+});
